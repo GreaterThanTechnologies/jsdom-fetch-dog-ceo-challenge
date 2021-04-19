a8609ef87ac6e63f0fb84d839181295387712780
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-19 10:38:07 -0400

    commit tree c0ac8ae5f913f87b8920b5ef74a53cb2ea8309e2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
+// we need an empty array to hold the breeds
+let breeds = []
+
+// we need to delay the js from completing until the page has loaded
+document.addEventListener('DOMContentLoaded', function () {
+  loadImages();
+  loadBreedOption();
+});
+
+// declaring the variables and setting the functions
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(dogPicUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let newImageEl = document.createElement('img');
+  newImageEl.src = dogPicUrl;
+  container.appendChild(newImageEl);
+}
+
+function loadBreedOptions() {
+  const breedURL = "https://dog.ceo/api/breeds/list/all"
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results =>{
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function selectBreedsStaringWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.curser = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+}
+
 console.log('%c HI', 'color: firebrick')
